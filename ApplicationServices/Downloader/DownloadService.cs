4f35e71d5ea881feba645ed6e7b7a2a45d830540
Bhathiya Dasanayake <bhathiyamadusanka@gmail.com>
2022-06-25 14:51:49 +0530

    Add configuration file, implement http request

--- ApplicationServices/Downloader/DownloadService.cs
+++ ApplicationServices/Downloader/DownloadService.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -7,9 +9,22 @@ namespace ApplicationServices.Downloader
 {
     public class DownloadService : IDownloadService
     {
+        private readonly IConfiguration _configuration;
+        private readonly IHttpClientFactory _httpClientFactory;
+        public DownloadService(IConfiguration configuration, IHttpClientFactory httpClientFactory)
+        {
+            _configuration = configuration;
+            _httpClientFactory = httpClientFactory;
+        }
         public async Task StartDownload()
         {
-            Console.WriteLine("Download Started");
+            var baseUrl = _configuration.GetSection("WebAppUrl:BaseUrl").Get<string>();
+            var httpResponse = await _httpClientFactory.CreateClient().GetAsync(baseUrl);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var responseString = await httpResponse.Content.ReadAsStringAsync();
+            }
         }
     }
 }
