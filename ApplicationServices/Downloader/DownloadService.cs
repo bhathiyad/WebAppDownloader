2a956dc693732c0017db38d33a692c5df4845538
Bhathiya Dasanayake <bhathiyamadusanka@gmail.com>
2022-06-26 14:03:18 +0530

    merge

--- ApplicationServices/Downloader/DownloadService.cs
+++ ApplicationServices/Downloader/DownloadService.cs
@@ -1,15 +1,126 @@
-﻿using System;
+﻿using ApplicationServices.Downloader.Models;
+using ApplicationServices.File;
+using ApplicationServices.RequestManager;
+using Microsoft.Extensions.Configuration;
+using Shared.Helpers;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace ApplicationServices.Downloader
 {
     public class DownloadService : IDownloadService
     {
+        private readonly IConfiguration _configuration;
+        private readonly IRequestManagerService _requestManagerService;
+        private readonly IFileService _fileService;
+        private readonly List<string> visitedUrls = new List<string>();
+        private readonly string baseUrl;
+        private readonly string mainDirectoryPath;
+        private readonly List<FileModel> fileModels = new List<FileModel>();
+
+        public DownloadService(IConfiguration configuration
+            , IRequestManagerService requestManagerService
+            , IFileService fileService)
+        {
+            _configuration = configuration;
+            _requestManagerService = requestManagerService;
+            _fileService = fileService;
+            baseUrl = _configuration.GetSection("WebAppUrl:BaseUrl").Get<string>();
+            mainDirectoryPath = _configuration.GetSection("Directories:MainDirectory").Get<string>();
+        }
         public async Task StartDownload()
         {
-            Console.WriteLine("Download Started");
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            Console.WriteLine("File download started...");
+
+            var urls = new List<string>() { "" };
+            Process(urls);
+            var fileModeltasks = fileModels.Select(fileModel => _fileService.SaveFileToDisk(fileModel));
+            await Task.WhenAll(fileModeltasks);
+
+            stopWatch.Stop();
+            Console.WriteLine("File download Completed...");
+            Console.WriteLine($"{visitedUrls.Count} Urls processed");
+            Console.WriteLine(stopWatch.Elapsed);
         }
+
+        private void Process(List<string> urls)
+        {
+            Parallel.ForEach(urls, url =>
+            {
+                if (!visitedUrls.Contains(url) && !url.Contains("http"))
+                {
+                    var response = _requestManagerService.GetWebPageContentNotAsync(baseUrl, url);
+
+                    visitedUrls.Add(url);
+
+                    if (!response.httpResponseMessage.RequestMessage.RequestUri.ToString().Contains("404"))
+                    {
+                        fileModels.Add(new FileModel
+                        {
+                            FileContent = response.httpResponseString,
+                            Directory = mainDirectoryPath,
+                            Url = url
+                        });
+
+                        //await Task.Run(() => SaveFile(responseString, url, mainDirectory)).ConfigureAwait(false);
+
+                        var newUrls = HelperExtensions.GetNewUrls(response.httpResponseString);
+                        Console.WriteLine($"Found new urls {newUrls.Count}");
+
+                        if (newUrls.Count > 0)
+                        {
+                            Process(newUrls);
+                        }
+                    }
+                }
+            });
+
+            //foreach (var url in urls)
+            //{
+            //    if (!visitedUrls.Contains(url) && !url.Contains("http"))
+            //    {
+            //        var response = await _requestManagerService.GetWebPageContent(baseUrl, url);
+
+            //        visitedUrls.Add(url);
+
+            //        if (response.httpResponseMessage.RequestMessage.RequestUri.ToString().Contains("404"))
+            //        {
+            //            continue;
+            //        }
+
+            //        fileModels.Add(new FileModel
+            //        {
+            //            FileContent = response.httpResponseString,
+            //            Directory = mainDirectoryPath,
+            //            Url = url
+            //        });
+
+            //        //await Task.Run(() => SaveFile(responseString, url, mainDirectory)).ConfigureAwait(false);
+
+            //        var newUrls = HelperExtensions.GetNewUrls(response.httpResponseString);
+            //        Console.WriteLine($"Found new urls {newUrls.Count}");
+
+            //        if (newUrls.Count > 0)
+            //        {
+            //            await Process(newUrls);
+            //        }
+            //        else
+            //        {
+            //            break;
+            //        }
+            //    }
+            //}
+        }
+
     }
+
 }
---
+++ ApplicationServices/RequestManager/RequestManagerService.cs
@@ -0,0 +1,67 @@
+﻿using ApplicationServices.RequestManager.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplicationServices.RequestManager
+{
+    public class RequestManagerService : IRequestManagerService
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RequestManagerService(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<ResponseModel> GetWebPageContent(string baseUrl, string urlPath)
+        {
+            var url = !string.IsNullOrWhiteSpace(urlPath) ?
+                $"{baseUrl}{urlPath[9..(urlPath.Length - 1)]}" : baseUrl;
+
+            Console.WriteLine($"Processing url : {url}");
+
+            var httpResponse = await _httpClientFactory.CreateClient().GetAsync(url).ConfigureAwait(false);
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var responseString = await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+                var httpResponseModel = new ResponseModel
+                {
+                    httpResponseMessage = httpResponse,
+                    httpResponseString = responseString
+                };
+
+                return httpResponseModel;
+            }
+
+            return null;
+        }
+
+        public ResponseModel GetWebPageContentNotAsync(string baseUrl, string urlPath)
+        {
+
+            var url = !string.IsNullOrWhiteSpace(urlPath) ?
+                $"{baseUrl}{urlPath[9..(urlPath.Length - 1)]}" : baseUrl;
+
+            Console.WriteLine($"Processing url : {url}");
+
+            var httpResponse = _httpClientFactory.CreateClient().GetAsync(url).Result;
+
+            if (httpResponse.IsSuccessStatusCode)
+            {
+                var responseString = httpResponse.Content.ReadAsStringAsync().Result;
+                var httpResponseModel = new ResponseModel
+                {
+                    httpResponseMessage = httpResponse,
+                    httpResponseString = responseString
+                };
+
+                return httpResponseModel;
+            }
+
+            return null;
+        }
+    }
+}
